db20fcbf56c7f4007b4013d6a7d34f6b14c576dd
Rachelooo <91844200+Rachelooo@users.noreply.github.com>
2022-04-09 08:06:31 -0400

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -1,3 +1,18 @@
+player_1= localStorage.getItem("player_1");
+player_2= localStorage.getItem("player_2");
+
+player1_score=0;
+player2_score=0;
+
+document.getElementById("player1name").innerHTML= player_1 + " : ";
+document.getElementById("player2name").innerHTML= player_2 + " : ";
+
+document.getElementById("player1score").innerHTML= player1_score;
+document.getElementById("player2score").innerHTML= player2_score;
+
+document.getElementById("questionturn").innerHTML= "Question Turn - " + player_1;
+document.getElementById("answerturn").innerHTML= "Answer Turn - " + player_2;
+
 function send(){
     number1=document.getElementById("word1").value;
     number2=document.getElementById("word2").value;
@@ -10,4 +25,40 @@ function send(){
     document.getElementById("output").innerHTML = row;
     document.getElementById("word1").value = "";
     document.getElementById("word2").value = "";
+}
+question_turn="player_1";
+answer_turn="player_2";
+
+function check(){
+    get_answer=document.getElementById("answerbox");
+    if(get_answer== actual_answer)
+    {
+        if(answer_turn=="player_1")
+        {
+            player1_score=player1_score+1
+            document.getElementById("player1score").innerHTML= player1_score;
+        }
+        else
+        {
+            player2_score=player2_score+1;
+            document.getElementById("player2score").innerHTML= player2_score;
+        }
+    }
+    if(question_turn=="player_1"){
+        question_turn="player_2";
+        document.getElementById("questionturn").innerHTML="Question turn - " + player_2;
+    }
+    else{
+        question_turn="player_1";
+        document.getElementById("questionturn").innerHTML="Question turn - " + player_1;
+    }
+    if(answer_turn=="player_1"){
+        answer_turn="player_2";
+        document.getElementById("answerturn").innerHTML="Answer turn - " + player_2;
+    }
+    else{
+        answer_turn="player_1";
+        document.getElementById("answerturn").innerHTML="Answer turn - " + player_1;
+    }     
+    document.getElementById("output").innerHTML="";
 }
